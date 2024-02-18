2282645a1898b57e0f311559bfbea302d368c1c2
Pascal S. de Kloe <pascal@quies.net>
2024-02-18 14:58:43 +0100

    benchmark separated from test, in main classpath, enables assertions on unit tests

--- README.md
+++ README.md
@@ -3,8 +3,11 @@
 A simplified order book implementation written in Java.
 This project is for personal exploration and learnings.
 
-The following was measured on an Apple M1 with OpenJDK 21.
+Run `mvn verify` for a benchmark. The following results were measured on an
+Apple M1 with Oracle JDK 21.
 
 ```
-8,4 M order places in 33,6 M packets took 666 ns on average
+8,4 M random order placements in 33,6 M packets took 668 ns on average
+16,6 M simulated order placements and 0,0 M order cancelations took 185 ns on average with 262144 open orders
+16,3 M simulated order placements and 0,0 M order cancelations took 186 ns on average with 1048576 open orders
 ```
--- src/main/java/order/BookBench.java
+++ src/main/java/order/BookBench.java
@@ -8,21 +8,26 @@ import java.util.Random;
 import static java.lang.System.nanoTime;
 import static java.nio.ByteOrder.nativeOrder;
 
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertSame;
-import static org.junit.Assert.assertTrue;
 
+/** Benchmark tests report to standard output. */
+public class BookBench {
 
-public class BookTest {
+	public static void main(String[] args) {
+		try {
+			placeRandom();
+			System.gc();
+			placeAndCancelInBookSize(256 * 1024);
+			placeAndCancelInBookSize(1024 * 1024);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 	/**
 	 * Random packets are the worst-case scenario in terms of performance.
 	 * Note that there are no illegal values in the packet format.
 	 */
-	@Test
-	public void loadRandom() throws IOException {
+	static void placeRandom() throws IOException {
 		// data close to maximum array size from Java
 		final int packetCount = 32 * 1024 * 1024;
 		// chance of buy place is 1 in 4 (with 2 flags applicable)
@@ -37,40 +42,42 @@ public class BookTest {
 		while (b.load());
 		long took = nanoTime() - start;
 
-		System.out.printf("%.1f M order places in %.1f M packets took %.0f ns on average\n",
-			(double)b.buyPlaceCount / 1000000,
+		long placeCount = b.buyPlaceCount + b.sellPlaceCount;
+		System.out.printf("%.1f M random order placements in %.1f M packets took %.0f ns on average\n",
+			(double)placeCount / 1000000,
 			(double)packetCount / 1000000,
-			(double)took / b.buyPlaceCount);
+			(double)took / placeCount);
 	}
 
 	/**
-	 * Test orders on an exponential scale to simulate more closely to a
+	 * Test orders with a logarthmic price distribution to simulate the
 	 * production environment. The number of buy orders is kept stable with
 	 * placements and cancelations on every other packet.
+	 * @param bookSize the number of orders in the book during testing.
 	 */
-	@Test
-	public void loadExpOnQuartMillion() throws IOException {
-		final int bookSize = 256 * 1024;
+	static void placeAndCancelInBookSize(final int bookSize) throws IOException {
+		if (Long.bitCount(bookSize) != 1)
+			throw new IllegalArgumentException("book size not a power of two");
 
 		ByteBuffer packetData = ByteBuffer.allocate(1<<30);
+		if (Book.PACKET_SIZE % 8 != 0)
+			throw new AssertionError("packet size not a multiple of 64 bits");
 		final long packetCount = packetData.limit() / Book.PACKET_SIZE;
-
-		// packet format is 64-bit aligned
 		LongBuffer words = packetData.order(nativeOrder()).asLongBuffer();
-		assert(Book.PACKET_SIZE == 32);
 		final int wordsPerPacket = Book.PACKET_SIZE / 8;
 
 		// random data, yet consistent for stable measures
 		Random r = new Random(123);
 
 		// order placement packets
-		for (int i = 0; i < bookSize; i++) {
+		for (int i = 0; i < packetCount; i++) {
 			// First bookSize count all create for initial data set.
 			// Then create only in even packet indices.
 			if (i >= bookSize && (i & 1) != 0) continue;
 
 			double price = 99 * Math.log(4 * r.nextDouble());
 
+			// TODO: add sell placements
 			words.put(i * wordsPerPacket, Book.BUY_NOT_SELL_FLAG | Book.PLACE_NOT_CANCEL_FLAG);
 			words.put((i * wordsPerPacket) + 1, (long)i);      // order ID
 			words.put((i * wordsPerPacket) + 2, r.nextLong()); // order amount
@@ -78,13 +85,14 @@ public class BookTest {
 		}
 
 		// order cancelation packets in odd indices after initial data set
-		assertTrue("even number for initial data set size", bookSize % 2 == 0);
+		if (bookSize % 2 != 0) throw new AssertionError("odd number for initial data set size");
 		for (int i = bookSize + 1; i < packetCount; i += 2) {
 			// random packet from past (could dupe)
 			int target = (int)(r.nextDouble() * i);
-			// even indices only for order places and no cancels
+			// target even indices for placements and no cancelations
 			target &= ~1;
 
+			// TODO: add sell cancelations
 			words.put(i * wordsPerPacket, Book.BUY_NOT_SELL_FLAG);
 			// match order ID, order amount and limit price
 			words.put((i * wordsPerPacket) + 1, words.get((target * wordsPerPacket) + 1));
@@ -95,27 +103,37 @@ public class BookTest {
 		Book b = new Book(new ByteArrayInputStream(packetData.array()));
 
 		// load initial data set
-		assertTrue(String.format("read buffer size %d a multiple of packet size %d", Book.READ_SIZE, Book.PACKET_SIZE),
-			Book.READ_SIZE % Book.PACKET_SIZE == 0);
-		int packetsPerRead = Book.READ_SIZE / Book.PACKET_SIZE;
-		assertTrue(String.format("book size %d a multiple of %d packets per read", bookSize, packetsPerRead),
-			bookSize % packetsPerRead == 0);
+		if (Book.READ_SIZE % Book.PACKET_SIZE != 0)
+			throw new AssertionError(
+				String.format("read buffer size %d not a multiple of packet size %d",
+					Book.READ_SIZE, Book.PACKET_SIZE));
+		final int packetsPerRead = Book.READ_SIZE / Book.PACKET_SIZE;
+		if (bookSize % packetsPerRead != 0)
+			throw new AssertionError(
+				String.format("book size %d not a multiple of %d packets per read",
+					bookSize, packetsPerRead));
 		for (int n = bookSize / packetsPerRead; --n >= 0; )
 			b.load();
-		assertEquals("buy places in initial data set", bookSize, b.buyPlaceCount);
-		assertEquals("buy cancels in initial data set", 0, b.buyCancelCount);
+		if (b.buyPlaceCount != bookSize)
+			throw new AssertionError(
+				String.format("loaded %d out of %d placements for initial data set",
+					b.buyPlaceCount, bookSize));
+		// TODO: implement cancelation:
+		if (b.buyCancelCount != 0)
+			throw new AssertionError("loaded cancelations in initial data set");
 
 		// benchmark processing time
 		long start = nanoTime();
 		while (b.load());
 		long took = nanoTime() - start;
 
-		long buyPlaceCount = b.buyPlaceCount - (long)bookSize;
-		long buyCancelCount = b.buyCancelCount;
-		System.out.printf("%.1f M order placements and %.1f M order cancelations took %.0f ns on average\n",
-			(double)buyPlaceCount / 1000000,
-			(double)buyCancelCount / 1000000,
-			(double)took / (double)(buyPlaceCount + buyCancelCount));
+		long placeCount = (b.buyPlaceCount + b.sellPlaceCount) - (long)bookSize;
+		long cancelCount = b.buyCancelCount + b.sellCancelCount;
+		System.out.printf("%.1f M simulated order placements and %.1f M order cancelations took %.0f ns on average with %d open orders\n",
+			(double)placeCount / 1000000,
+			(double)cancelCount / 1000000,
+			(double)took / (double)(placeCount + cancelCount),
+			bookSize);
 	}
 
 }
